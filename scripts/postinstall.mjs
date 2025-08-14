c60a1398d132ff249d5a2510bf9fa109ef99761b
Filip Sobol <filipsobol@users.noreply.github.com>
2025-08-14 11:08:07 +0200

    Update `postinstall.mjs` script.

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -3,26 +3,13 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
  */
 
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
+import { join } from 'path';
 import { existsSync } from 'fs';
 
-const ROOT_DIRECTORY = join(
-	dirname( fileURLToPath( import.meta.url ) ),
-	'..'
-);
+// When installing a repository as a dependency, the `.git` directory does not exist.
+// In such a case, husky should not attach its hooks as npm treats it as a package, not a git repository.
+if ( existsSync( join( import.meta.dirname, '..', '.git' ) ) ) {
+	const { default: install } = await import( 'husky' );
 
-main()
-	.catch( err => {
-		console.error( err );
-	} );
-
-async function main() {
-	// When installing a repository as a dependency, the `.git` directory does not exist.
-	// In such a case, husky should not attach its hooks as npm treats it as a package, not a git repository.
-	if ( existsSync( join( ROOT_DIRECTORY, '.git' ) ) ) {
-		const { install } = await import( 'husky' );
-
-		install();
-	}
+	install();
 }
